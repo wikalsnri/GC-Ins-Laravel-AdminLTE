08cbf597e4a4d60d3d84a284fe81cc1b3b0919ec
wikalsnri <wikalsnri@gmail.com>
2023-10-04 20:02:39 +0700

    tes

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,7 +1,7 @@
 <html lang="en" style="height: auto;"><head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>@yield('title', 'AdminLTE Template')</title>
+  <title>@yield('title', 'AdminLTE Template') Wika</title>
 
 
   <link rel="stylesheet" href="{{ asset('css/adminlte.css') }}">
